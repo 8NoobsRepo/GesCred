2c9f8c27ba6fbb67b98053cdc7f51a4ff0dcdb57
Onelog Oficial <onelog@protonmail.ch>
2016-08-30 16:52:03 -0500

    Actualizacion del Repo

---
+++ GesCred/GesCred.py
@@ -0,0 +1,16 @@
+'''
+Proyecto GesCred
+----------------
+Descripcion: Gestor de Credenciales, aplicacion para la administracion
+    de ususarios y claves de accesos de tus cuentas personal.
+    El proyecto fue programado en Visual Studio 2015 Community.
+Autor: Sergio Marquez (OneLog - onelog@protonmail.ch)
+Fecha: 29/08/2016
+Version: 1.0.0
+'''
+
+import tkinter
+import colorsys
+
+print('#' * 80, end = '\n')
+print('Hola Mundo')
